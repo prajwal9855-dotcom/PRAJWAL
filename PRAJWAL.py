0f0238a791d50c9cb693055be84d704e818477df
prajwal9855-dotcom <prajwal9855@gmail.com>
2026-01-20 15:28:24 +0530

    Update PRAJWAL.py

--- PRAJWAL.py
+++ PRAJWAL.py
@@ -2,7 +2,7 @@ import streamlit as st
 from groq import Groq
 
 st.set_page_config("PragyanAI Content Generator", layout="wide")
-st.title("📢 darshanAI – Content Generator")
+st.title("📢 prajwalAI – Content Generator")
 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
